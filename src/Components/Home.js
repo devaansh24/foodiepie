a3cdb729c48d133776aa420fdcfb334e3f6b1d23
Devansh Sharma <devanshsharma@Devanshs-MacBook-Air.local>
2023-07-12 01:40:48 +0530

    search working

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import resList from "../datafile";
 import Restaurants from "./Restaurants";
+import "./Home.css"
 
 const Home = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredData = resList.filter((item, index) => {
+    const currentName = item.data.name.toLowerCase();
+    const searchTerm = search.toLowerCase();
+    const isDuplicate = resList.findIndex(
+      (res, idx) => res.data.name.toLowerCase() === currentName && idx < index
+    );
+    return currentName.includes(searchTerm) && isDuplicate === -1;
+  });
+
   return (
     <div className="body">
-          <div className="search">Search</div>
-          
+      <div className="search">
+        <input
+          type="text"
+          value={search}
+          onChange={handleSearch}
+          placeholder="Search restaurants..."
+        />
+      </div>
+
       <div className="res-container">
-        {resList.map((restaurant) => {
-          return <Restaurants resData={restaurant} />;
-        })}
+        {filteredData.map((restaurant) => (
+          <Restaurants key={restaurant.data.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
